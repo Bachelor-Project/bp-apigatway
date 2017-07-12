dc312cf3816469ba777dbb7eeaf99e163af79dd0
dato-qobuladze <davidkobuladzemail@gmail.com>
2017-07-12 22:13:16 +0400

    FileUpload

--- bp-APIGatway/src/main/java/com/mycompany/bp/apigatway/AppConfig.java
+++ bp-APIGatway/src/main/java/com/mycompany/bp/apigatway/AppConfig.java
@@ -5,19 +5,48 @@
  */
 package com.mycompany.bp.apigatway;
 
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
  *
  * @author Dato
  */
-@ApplicationPath("api")
-public class AppConfig extends ResourceConfig {
-    
-    public AppConfig(){
-        super();
-        
-        register(APIService.class);
+@ApplicationPath("/api")
+public class AppConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        resources.add(MultiPartFeature.class);
+        addRestResourceClasses(resources);
+        return resources;
     }
+    
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.mycompany.bp.apigatway.APIService.class);
+        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+    }
+    
+//    public AppConfig(){
+//        super();
+//        
+//        register(APIService.class);
+//    }
 }
+
+
+
+
+
+//public class AppConfig extends ResourceConfig {
+//    
+//    public AppConfig(){
+//        super();
+//        
+//        register(APIService.class);
+//    }
+//}
